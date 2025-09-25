f156d66fe3d1e53b0d3508add3c204411f0e2677
Theodore Willingham <tsw91905@uga.edu>
2025-09-25 13:30:51 -0400

    fixed arguments in api/items/id

--- my-app/src/app/api/items/[id]/route.ts
+++ my-app/src/app/api/items/[id]/route.ts
@@ -1,34 +1,49 @@
 import connectMongoDB from "../../../../../config/mongodb";
 import mongoose from "mongoose";
 import Item from "@/app/models/itemSchema";
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-//handles endpoints for specific items(in /api/items/[id]/route.ts)
+
+// GET, PUT, DELETE for /api/items/[id]
 
 interface RouteParams {
-    params: { id: string };
+    // params: { id: string };
+    id: string;
+}
+
+interface HandlerProps {
+    params: RouteParams;
 }
 
 // Get's unique item
-export async function GET(request: NextRequest, { params }:RouteParams) {
+export async function GET(request: NextRequest, { params }:HandlerProps) {
     const { id } = await params; //Get the items ID
     await connectMongoDB();
     const item = await Item.findOne({ _id: id }); //finds item with given ID
+
+    if (!item) {
+        return NextResponse.json({ message: "Item not found" }, { status: 404 });
+    }
+
     return NextResponse.json({ item }, { status: 200 });
 }
 
 // Update's unique item
-export async function PUT(request: NextRequest, { params }:RouteParams) {
+export async function PUT(request: NextRequest, { params }:HandlerProps) {
     const { id } = await params; //Get the items ID
     const { name: name, price: price, location: location, lat: lat, lon: lon, imageUrl: imageUrl, category: category } = await request.json(); 
     await connectMongoDB()
-    await Item.findByIdAndUpdate(id,  { name, price, location, lat, lon, imageUrl, category }); 
+    const updatedItem = await Item.findByIdAndUpdate(id,  { name, price, location, lat, lon, imageUrl, category }); 
+
+    if (!updatedItem) {
+        return NextResponse.json({ message: "Item not found" }, { status: 404 });
+    }
+
     return NextResponse.json({ message: "Item Updated" }, { status: 200 });
 }
 
 // Delte's unique item
-export async function DELETE(request: NextRequest, { params }: RouteParams) {
+export async function DELETE(request: NextRequest, { params }: HandlerProps) {
     const { id } = await params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) { //if Id isn't valid
