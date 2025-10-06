81202bbdce99dc946c7b120ad8f7edfe305a18b3
Theodore Willingham <tsw91905@uga.edu>
2025-10-05 21:02:40 -0400

    Started Creating portfolio

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,30 +1,22 @@
 "use client";
 
-import React from "react";
-// import { useRouter } from "next/navigation";
-import Image from "next/image"
-import "./styles/Home.css";
+import React from "react"; 
 
 export default function Home() {
   // const router = useRouter();
 
-  const handleEnterSite = () => {
+  const goToMarketPlace = () => {
     // router.push("/main"); // Navigate to the original page.tsx (now at /main)
     window.location.href = "/main"; //This hard resets the page and fixes styling errors
   };
 
   return (
     <div className="home-container">
-      {/* <button className="enter-button">
-        Sign In
-      </button> */}
-      <Image src="/assets/logo.png" alt="Bulldog Market Logo" className="logo" width={200} height={200} />
-      <h1>Welcome to Bulldog Market</h1>
       <p>
         This is my portoflio
       </p>
-      <button onClick={handleEnterSite} className="enter-button">
-        Go To UGA DAWGS
+      <button onClick={goToMarketPlace} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        Go To UGA BullDawg Marketplace
       </button>
     </div>
   );
